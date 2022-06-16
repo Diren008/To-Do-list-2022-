b9f8a522123eb2b2243822ea98504d9664005248
Diren008 <98965434+Diren008@users.noreply.github.com>
2022-06-16 15:59:16 +0200

    Java Begining

---
+++ JavaScript/javascript.js
@@ -0,0 +1,5 @@
+//Variables of main elements
+const taskInput=document.getElementsByClassName("newTasks");
+const buttonAdd=document.getElementsByClassName("button")[0];
+const incompleteTask=document.getElementsById("toDoTasks");
+const completedTask=document.getElementById("completedTasks");
